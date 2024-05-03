09b00be73d066603dd094333bd60897f2221f7bc
Chethan R. <chethan.r@excelindia.com>
2024-05-03 09:16:10 +0530

    changes made by chethan

---
+++ react-js/to-do/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import UseTodo from './components/UseTodo';
+
+function App() {
+  return (
+    <div className="App">
+      <UseTodo/>
+    </div>
+  );
+}
+
+export default App;
